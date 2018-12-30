d2b538f2ac47aad0baf0532eb4a4d671d6616b67
Thiery Emanuel <malik.trueheart@gmail.com>
2018-12-30 22:16:55 +0200

    test

--- AutomatizareCasa/app/src/main/java/ro/thich/acasa/MainActivity.java
+++ AutomatizareCasa/app/src/main/java/ro/thich/acasa/MainActivity.java
@@ -1,13 +1,15 @@
 package ro.thich.acasa;
 
 import android.app.*;
-import android.content.res.*;
 import android.os.*;
-import android.support.v4.widget.*;
-import android.support.v7.app.*;
 import android.view.*;
 import android.view.View.*;
 import android.widget.*;
+import java.io.*;
+import org.apache.http.*;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
 
 
 public class MainActivity extends Activity 
@@ -68,6 +70,8 @@ public class MainActivity extends Activity
 				@Override
 				public void onClick(View v) {
 					Toast.makeText(MainActivity.this, "Baia Mare", Toast.LENGTH_SHORT).show();
+					new RequestTask().execute("http://stackoverflow.com");
+					
 
 				}
 			});
@@ -159,6 +163,41 @@ public class MainActivity extends Activity
 		}
 	}
 	
+}
+
+class RequestTask extends AsyncTask<String, String, String>{
+
+    @Override
+    protected String doInBackground(String... uri) {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response;
+        String responseString = null;
+        try {
+            response = httpclient.execute(new HttpGet(uri[0]));
+            StatusLine statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                responseString = out.toString();
+                out.close();
+            } else{
+                //Closes the connection.
+				response.getEntity().getContent().close();
+                throw new IOException(statusLine.getReasonPhrase());
+            }
+        } catch (ClientProtocolException e) {
+            //TODO Handle problems..
+        } catch (IOException e) {
+            //TODO Handle problems..
+        }
+        return responseString;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        //Do anything with response..
+    }
 }
 
 
